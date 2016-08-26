7fc135f46e170c2924464656f2fff9bb807c5d0c
nobert2016 <jumaevan2030@yahoo.com>
2016-08-26 23:18:58 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -2,33 +2,21 @@ str_file = "cities.txt"
 
 def save_cities(city, county, population):
     file = open(str_file, "a")
-    file.write("%16s%16s%16f\n"%(city,county,population))
+    file.write("%16s%16s%16f\n"%(city, county, population))
     file.close()
 
 def read_cities():
     file = open(str_file, "r")
     for line in file:
-        (city,county,population) = line.split()
-        print("%s"%(city)+"   "+"%s"%(county)+" "+"%f"%float(population))
-
-"""def most_populated():
-    highest_population = 0
-    population = open("cities.txt")
-    for line in population:
-        if float(line) > highest_population:
-          highest_population = float(line)
-        population.close()
-        print("The most populated City is: ")
-        print(highest_population)
-"""
-
+        (city, county, population) = line.split()
+        print("%s"%(city)+" "+"%s"%(county)+" "+"%f"%float(population))
 
 print("Data of Kenyan Cities")
 print(">>>>>>>>>>>>>>>>>>>>>")
 print("1. Add a City")
 print("2. View the Cities")
-print("3. Most Populated City")
-print("4. Least Populated City")
+print("3. Most and Least Populated Cities")
+print("4. Top 5 most Populated")
 str_choice = input("Choose an Option from the above Menu: ")
 choice = int(str_choice)
 
@@ -36,11 +24,31 @@ if(choice == 1):
     city = input("Enter city: ")
     county = input("Enter county: ")
     population_str = input("Enter population: ")
-    #county = float(county)
     population_str = float(population_str)
     save_cities(city,county,population_str)
 elif(choice ==2):
     read_cities()
-#else:(choice == 3)
-#most_populated()
+elif(choice ==3):
+    def conversion(num):
+        try:
+            num=float(num)
+            return True
+        except Exception:
+            return False
+    with open('cities.txt', 'r') as f:
+       numbers=[float(num) for line in f for num in line.split() if conversion(num)]
+    minim = 99999999999999
+    maximum= -99999999999999
+    if numbers:
+        if max(numbers)>maximum:
+            maximum=max(numbers)
+        if min(numbers)<minim:
+           minim=min(numbers)
+           print("Most Populated City:" + str(maximum))
+           print ("Least Populated City:" + str(minim))
+elif(choice == 4):
+    print("The Top 5 Most Populated Cities")
+
+
+
 
